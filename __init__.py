0697acd06a130523e6fdba7d28d32bed71713cd2
YunMengAI <vkelly700@gmail.com>
2026-06-11 23:27:29 +0800

    Register YM GPT Image 2 node

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""YM GPT Image 2.0 ComfyUI custom node."""
+
+from .ym_gpt_image2 import YM_GPT_image2_0
+
+NODE_CLASS_MAPPINGS = {
+    "YM_GPT_image2_0": YM_GPT_image2_0,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "YM_GPT_image2_0": "YM_GPT_image2.0",
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
